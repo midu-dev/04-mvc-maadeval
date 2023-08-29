9ed1aa308d32d8bd2e67dfbd2b29617359f6660a
madeval <alvarez.fing@gmail.com>
2023-08-28 21:15:00 -0300

    feat: :sparkles: add solucion

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,16 +1,29 @@
 // importar modelo
+import { TaskModel, TaskNotFoundError } from '../models/task.js'
 
-export const getAllTasks = (req, res) => {
-  const tasks = // ????
+export const getAllTasks = async (req, res) => {
+  const tasks = await TaskModel.getAllTasks()
   res.json(tasks)
 }
 
-export const addTask = (req, res) => {
-  const task = // ????
+export const addTask = async (req, res) => {
+  const { description } = req.body
+  const task = await TaskModel.addTask({ description })
+
   res.json(task)
 }
 
-export const deleteTask = (req, res) => {
-  // ????
+export const deleteTask = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    await TaskModel.deleteTask({ id: Number(id) })
+  } catch (err) {
+    if (err instanceof TaskNotFoundError) {
+      return res.status(404).json({ message: err.message })
+    }
+    return res.status(500).json({ message: err.message })
+  }
+
   res.json({ message: 'Deleted successfully' })
 }
--- models/task.js
+++ models/task.js
@@ -27,16 +27,50 @@ let currentId = 1
    */
 
 /**
-   * Adds a new task to the list.
-   * @name TaskModel.addTask
-   * @static
-   * @param {string} description - The description of the task.
-   * @returns {Object} The new task object.
-   */
+ * Adds a new task to the list.
+ * @name TaskModel.addTask
+ * @static
+ * @param {string} description - The description of the task.
+ * @returns {Object} The new task object.
+ */
 
 /**
-   * Deletes a task from the list by its ID.
-   * @name TaskModel.deleteTask
-   * @static
-   * @param {number} id - The ID of the task to delete.
-   */
+ * Deletes a task from the list by its ID.
+ * @name TaskModel.deleteTask
+ * @static
+ * @param {number} id - The ID of the task to delete.
+ */
+export class TaskModel {
+  static async getAllTasks() {
+    return tasks
+  }
+
+  static async addTask({ description }) {
+    const newTask = {
+      id: currentId++,
+      description,
+    }
+
+    tasks.push(newTask)
+
+    return newTask
+  }
+
+  static async deleteTask({ id }) {
+    const indexTaskFound = tasks.findIndex((task) => task.id === id)
+
+    const isTaskNotFound = indexTaskFound === -1
+    if (isTaskNotFound) {
+      throw new TaskNotFoundError(`Task with id:${id} not found`)
+    }
+
+    tasks.splice(indexTaskFound, 1)
+  }
+}
+
+export class TaskNotFoundError extends Error {
+  constructor(message) {
+    super(message)
+    this.name = 'TaskNotFoundError'
+  }
+}
